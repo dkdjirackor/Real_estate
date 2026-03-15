0a7f6544afa561ffec4b9cacf08d21c6d54453c7
dkdjirackor <nathanieldjirac@icloud.com>
2026-03-15 14:43:40 +0000

    refactor: clean up imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Homepage from './pages/homepage'
+import Homepage from './pages/Homepage'
 
 function App() {
 
